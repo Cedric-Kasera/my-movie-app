d56b231dfcc513e4012b9bacb8b84e7c13c790e2
Omar Kasera <omarkasera50@gmail.com>
2024-10-30 17:07:17 +0300

    First Commit.

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../Components/Common/Button'
+
+function Home() {
+  return (
+    <div>
+      <Button title="submit" color="red" sizes="small"/>
+    
+    </div>
+  )
+}
+
+export default Home
